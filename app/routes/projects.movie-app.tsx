a820cb8dc036defea55e32f09deb959563111779
garancesan <garancesan@gmail.com>
2023-10-23 14:55:03 +0200

    movie.app done

--- app/routes/projects.movie-app.tsx
+++ app/routes/projects.movie-app.tsx
@@ -1,5 +1,6 @@
-import { type LinksFunction } from "@remix-run/node";
+import { type LinksFunction, json } from "@remix-run/node";
 import movieStyles from "~/styles/projects/movie.css";
+import { useLoaderData } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
   {
@@ -12,16 +13,38 @@ const API_URL =
   "https://api.themoviedb.org/3/discover/movie?language=fr&sort_by=popularity.desc&api_key=e0f4c27dbefdc34a10f79ca4c8ad0429&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL =
-  'https://api.themoviedb.org/3/discover/movie?api_key=e0f4c27dbefdc34a10f79ca4c8ad0429&query="';
+  'https://api.themoviedb.org/3/search/movie?api_key=e0f4c27dbefdc34a10f79ca4c8ad0429&query="';
 
-getMovies(API_URL);
-
-async function getMovies(url) {
-  const response = await fetch(url);
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const search = new URLSearchParams(url.search);
+  const query = search.get("q");
+  let API_URL =
+    "https://api.themoviedb.org/3/discover/movie?language=fr&sort_by=popularity.desc&api_key=e0f4c27dbefdc34a10f79ca4c8ad0429&page=1";
+  if (query) {
+    API_URL = `https://api.themoviedb.org/3/search/movie?api_key=e0f4c27dbefdc34a10f79ca4c8ad0429&query=${query}`;
+  }
+  const response = await fetch(API_URL);
   const responseData = await response.json();
-}
+  const { results } = responseData;
+  return json({
+    results,
+  });
+};
 
 export default function Movies() {
+  const { results } = useLoaderData<typeof loader>();
+
+  function getClassByRate(vote) {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 4) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  }
+
   return (
     <>
       <body>
@@ -30,25 +53,30 @@ export default function Movies() {
             <input
               type="text"
               id="search"
+              name="q"
               className="search"
               placeholder="Search"
             />
           </form>
         </header>
         <main id="main">
-          <div className="movie">
-            <img src="/imgs/movie.jpg" alt="movieImage" />
-            <div className="movie-info">
-              <h3>Movie title</h3>
-              <span className="green">9.8</span>
-            </div>
-            <div className="overview">
-              <h3>Overview</h3>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Reiciendis amet sed illum esse repellendus itaque perferendis iste
-              veniam, illo possimus.
-            </div>
-          </div>
+          {results.map((r) => {
+            return (
+              <div className="movie" key={r.id}>
+                <img src={`${IMG_PATH}${r.poster_path}`} alt="movieImage" />
+                <div className="movie-info">
+                  <h3>{r.title}</h3>
+                  <span className={getClassByRate(r.vote_average)}>
+                    {r.vote_average}
+                  </span>
+                </div>
+                <div className="overview">
+                  <h3>Overview</h3>
+                  <p>{r.overview}</p>
+                </div>
+              </div>
+            );
+          })}
         </main>
       </body>
     </>
